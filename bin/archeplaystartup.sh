1804f6721bb07058adc68264513e2d750cf2389b
Ubuntu <ubuntu@ip-172-31-49-31.ec2.internal>
2021-04-28 17:26:21 +0000

    ip command change

--- bin/archeplaystartup.sh
+++ bin/archeplaystartup.sh
@@ -3,8 +3,7 @@ echo "LOG From - "`date`
 sed -i 's/first-found/interface=ens*/g' /archeplay/package/microk8s/upgrade-scripts/000-switch-to-calico/resources/calico.yaml
 sed -i 's/hostPort: 443/hostPort: 9443/g' /archeplay/package/microk8s/actions/ingress.yaml
 sed -i 's/hostPort: 80/hostPort: 9000/g' /archeplay/package/microk8s/actions/ingress.yaml
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-machineip=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/local-ipv4`
+machineip=`curl -s ifconfig.me`
 grep -v 'advertise-address' /archeplay/package/microk8s/default-args/kube-apiserver >  /archeplay/package/microk8s/default-args/kube-apiserver.new
 mv  /archeplay/package/microk8s/default-args/kube-apiserver.new /archeplay/package/microk8s/default-args/kube-apiserver
 echo "--advertise-address=$machineip" >> /archeplay/package/microk8s/default-args/kube-apiserver
